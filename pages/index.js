1eabc9071dd590931cb2a338d04edb183e4790b1
Momin Kaleem <mominkaleem6@gmail.com>
2022-11-26 00:30:46 -0500

    Link Twitter and Tokium

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,11 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div class="flex flex-row justify-center items-center min-h-max h-screen">
-      <h1 class="hover:cursor-pointer">yo! 👋 my name is mo. currently building stuff i find interesting</h1>
+      <h1>
+        yo! 👋 my name is{" "}
+        <span class="hover:cursor-pointer underline text-sky-400"><a target="_blank" href="https://twitter.com/_nwmo" rel="noopener noreferrer">mo.</a></span>{" "}
+        currently building <span class="hover:cursor-pointer underline text-sky-400"><a target="_blank" href="https://tokium.co" rel="noopener noreferrer">Tokium</a></span> - a toolkit for NFT communities to royalty gate content.
+      </h1>
     </div>
   );
 }
